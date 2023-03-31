743eb3ae2bc764a7a63c83d5f390c3f50ee373cd
veresmar <veres.maria.dev@gmail.com>
2023-03-31 18:10:27 +0400

    new

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
+const wrapper = document.createElement('div');
+wrapper.className = 'wrapper';
+document.body.append(wrapper);
+
 let card = {
   emoji: '💯',
   title: '100',
   keywords: 'Hundred, points, symbol, wow, win, perfect, parties',
 }
 
-
 function createCard(obj) {
   const card = document.createElement('div');
   card.className = 'card';
 
-  const emoji = document.createElement('p');
+  const card_wrap = document.createElement('div');
+  card_wrap.className = 'card_wrap';
 
+  const emoji = document.createElement('p');
+  emoji.className = 'emoji';
+  emoji.textContent = '💯'
 
   const title = document.createElement('h2');
-  title.textContent = obj.name;
   title.className = 'title';
+  title.textContent = `${obj.title}`;
 
   const keywords = document.createElement('p');
+  keywords.className = 'keywords';
+  keywords.textContent = `${obj.keywords}`;
 
-  card.append(emoji, title, keywords);
+  card_wrap.append(emoji, title, keywords);
+  card.append(card_wrap);
   document.body.append(card);
-
 }
 
 createCard(card);
